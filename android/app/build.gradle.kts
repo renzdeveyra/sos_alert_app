8a1930ac5fafce9f1d6324308857afeac781f42b
IndigonaBlue <rdadeveyra@gmail.com>
2025-05-03 22:26:56 +0800

    updated pubspec

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,14 +18,11 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_18.toString()
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.sos_alert_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -34,8 +31,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -46,6 +41,5 @@ flutter {
 }
 
 dependencies {
-    // Add desugaring dependency
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
